bc8df0c2821f4b0442210c07336eaa51482f9586
febster2102 <nitchakarn.nil@student.mahidol.edu>
2026-03-16 08:50:26 +0000

    w10

--- main.cpp
+++ main.cpp
@@ -9,9 +9,15 @@ int main(){
     struct Time t1,t2,t3;
     cout<<"What time was it?";
     getTime(t1);
+
+
     cout<<"What time is it now?";
     getTime(t2);
-    t3=subtract(t2,t1); //t3=t2-t1
+
+
+    t3=substract(t2,t1); //t3=t2-t1
+
     cout<<"Time diff is ";
+
     display(t3);
 }
